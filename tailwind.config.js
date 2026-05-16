fdef6f18acb21be1f5e4a5ffa262477b91fb16a4
bbhtruonglm <truonglm.bbh@gmail.com>
2026-05-16 22:32:30 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,29 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/@bbhhainx/chat-core/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      borderRadius: {
+        '2.5xl': '20px',
+      },
       fontSize: {
         'xxs': '0.625rem', // Tương đương 10px
+        'xs-': '0.6875rem', // Tương đương 11px
+        'sm-': '0.8125rem', // Tương đương 13px
       },
       spacing: {
         '1.25': '0.3125rem',  // Tương đương 5px
         '1/2': '50%', // Thêm giá trị 50% vào thang đo spacing
-      }
+      },
+      lineHeight: {
+        '4.5': '1.125rem', // Tương đương 18px
+      },
+      screens: {
+        'lg+': '1200px',
+      },
     }
   },
   plugins: [
